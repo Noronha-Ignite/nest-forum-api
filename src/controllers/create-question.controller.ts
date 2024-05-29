89398a8f0d478fcafd0b740812dc0ccf183d5386
Gabriel Noronha <inc.691@gmail.com>
2024-05-29 01:16:40 -0300

    feat: implement decorator to get authorized user info

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { UserPayload } from 'src/auth/jwt.strategy'
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { z } from 'zod'
@@ -19,7 +21,7 @@ export class CreateQuestionController {
 
   @Post()
   @UsePipes(new ZodValidationPipe(CreateQuestionBodySchema))
-  async handle() {
-    return 'ok'
+  async handle(@CurrentUser() user: UserPayload) {
+    return user
   }
 }
