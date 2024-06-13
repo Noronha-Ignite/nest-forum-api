78636cb1ee75064126a7fa47c614809b4688558c
Gabriel Noronha <inc.691@gmail.com>
2024-06-13 01:48:53 -0300

    chore: create env module and replace config service uses with it

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,17 +1,19 @@
 import { ConfigModule } from '@nestjs/config'
 import { Module } from '@nestjs/common'
-import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { HttpModule } from './http/http.module'
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
 
 @Module({
   imports: [
-    AuthModule,
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
     HttpModule,
+    EnvModule,
   ],
 })
 export class AppModule {}
--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core'
-import { ConfigService } from '@nestjs/config'
 import { AppModule } from './app.module'
-import { Env } from './env'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  const configService: ConfigService<Env, true> = app.get(ConfigService)
-  const PORT = configService.get('PORT', { infer: true })
+  const envService = app.get(EnvService)
+  const PORT = envService.get('PORT')
 
   await app.listen(PORT)
 }
