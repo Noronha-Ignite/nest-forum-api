065d3f5b748036c35785ce2640f5fbfedae83e4e
Gabriel Noronha <inc.691@gmail.com>
2024-06-14 01:54:11 -0300

    test: create student and question persistent layer factory with prisma

--- src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,46 +1,40 @@
 import { AppModule } from '@/infra/app.module'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { DatabaseModule } from '@/infra/database/database.module'
 import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
 
 describe('Get question by slug (E2E)', () => {
   let app: INestApplication
-  let prisma: PrismaService
   let jwt: JwtService
+
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
 
-    prisma = moduleRef.get(PrismaService)
     jwt = moduleRef.get(JwtService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
 
     await app.init()
   })
 
   test('[GET] /questions/:slug', async () => {
-    const user = await prisma.user.create({
-      data: {
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: '123456',
-      },
-    })
-
-    const accessToken = jwt.sign({ sub: user.id })
-
-    await prisma.question.create({
-      data: {
-        title: 'Question 01',
-        slug: 'question-01',
-        content: 'Question 01 content',
-        authorId: user.id,
-      },
+    const user = await studentFactory.makePrismaStudent()
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+    await questionFactory.makePrismaQuestion({
+      title: 'Question 01',
+      authorId: user.id,
     })
 
     const response = await request(app.getHttpServer())
